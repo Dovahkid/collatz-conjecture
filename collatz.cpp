0be1ba4184d871b644fcf54aa555f8751c523ac9
MichaelG <gainmikey@gmail.com>
2021-08-16 19:27:38 -0400

    added folder input
    asks user to input the folder for the output files

--- collatz.cpp
+++ collatz.cpp
@@ -8,27 +8,35 @@
 
 using namespace std;
 
-unsigned long long calculation(unsigned long long, ofstream&);
+unsigned long long calculation(unsigned long long, ofstream&, int&);
 
 int main() {
 
 	unsigned long long min;
 	unsigned long long max;
+	int count = 0;
+	string folder;
 
 	cout << "Please enter a min a max space seperated: ";
 	cin >> min >> max;
 	cin.ignore();
+	
+	cout << "Enter a folder to store the outputs in: ";
+	cin >> folder;
+	cin.ignore();
 
 	for (unsigned long long i = min; i <= max; i++) {
-		ofstream fileOut("./collatz output/"+ to_string(i) + ".txt", ios_base::out);
+		ofstream fileOut(folder + "/"+ to_string(i) + ".txt", ios_base::out);
 		
 		if (!fileOut) {
 			cerr << "File could not be loaded! " << to_string(i) << endl;
 			continue;
 		}
 		cout << "Calculating: " << i << endl;		
-		calculation(i, fileOut);
-		fileOut << " " << endl;
+		calculation(i, fileOut, count);
+		fileOut << " \n" << count << endl;
+		
+		count = 0;
 		
 		fileOut.close();
 	}
@@ -42,19 +50,20 @@ int main() {
 	return 0;
 }
 
-unsigned long long calculation(unsigned long long num, ofstream& output) {
+unsigned long long calculation(unsigned long long num, ofstream& output, int& count) {
 	
 	output << num << endl;
+	count++;
 
 	if (num == 1) {
 		return 1;
 	}
 
 	if (num % 2 == 0) {
-		return calculation(num/2, output);
+		return calculation(num/2, output, count);
 	}
 	else {
-		return calculation( (3*num) + 1, output);
+		return calculation( (3*num) + 1, output, count);
 	}
 
 }
