ccc5e45b72c5012677b8a998392060da53fc7049
MichaelG <gainmikey@gmail.com>
2021-08-16 16:59:05 -0400

    user friendlyness
    asks for input and cleaner output

--- collatz.cpp
+++ collatz.cpp
@@ -1,3 +1,7 @@
+/*
+ * g++ collatz.cpp -o collatz.exe -static
+ * */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,20 +11,30 @@ using namespace std;
 unsigned long long calculation(unsigned long long, ofstream&);
 
 int main() {
-	
-	for (unsigned long long i = 1; i <= 1000000; i++) {
+
+	unsigned long long min;
+	unsigned long long max;
+
+	cout << "Please enter a min a max space seperated: ";
+	cin >> min >> max;
+
+	for (unsigned long long i = min; i <= max; i++) {
 		ofstream fileOut("./collatz output/"+ to_string(i) + ".txt", ios_base::app);
 		
 		if (!fileOut) {
 			cerr << "File could not be loaded! " << to_string(i) << endl;
 			continue;
 		}
-		
+		cout << "Calculating: " << i << endl;		
 		calculation(i, fileOut);
 		fileOut << " " << endl;
 		
 		fileOut.close();
 	}
+
+	cout << endl << "---------------" << endl;
+	cout << "     DONE!     " << endl;
+	cout << "---------------" << endl;
 
 	return 0;
 }
